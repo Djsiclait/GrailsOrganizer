8b85edfe454a3f423ff433b8d55c670af109b8b2
Djsiclait <djidjellypas92@gmail.com>
2016-11-21 18:29:07 -0400

    Still gives ERRORS

--- grails-app/init/grailsorganizer/Application.groovy
+++ grails-app/init/grailsorganizer/Application.groovy
@@ -2,7 +2,14 @@ package grailsorganizer
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.grails.boot.internal.EnableAutoConfiguration
+import org.springframework.boot.SpringApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
